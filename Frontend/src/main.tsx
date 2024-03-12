e51c83028509dd1453e6d2323560e72d08f0a842
Movindu Imeth Liyanage <142730691+Movin21@users.noreply.github.com>
2024-03-13 03:58:10 +0530

    firebase

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -2,8 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home/Home.tsx";
-
+import firebase from "firebase/compat/app";
 import "./index.css";
+
+const firebaseConfig = {};
+
+firebase.initializeApp(firebaseConfig);
 
 const router = createBrowserRouter([
   {

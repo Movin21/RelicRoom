a6c58fe350a971adb49979cba996d38092e1d189
Yasas Lakmina <138884690+YasasLakmina@users.noreply.github.com>
2024-03-13 12:40:09 +0530

    commit2

--- Backend/Server.js
+++ Backend/Server.js
@@ -3,15 +3,11 @@ const dotenv = require("dotenv").config();
 const connectDB = require("./DB Connection/DBConnection.js");
 const cors = require("cors");
 const errorHandler = require("./middleware/errorHandler.js");
-<<<<<<< Updated upstream
- const adminRouter = require("./Application/AdminPortal/routes/adminRoute.js");
-=======
 
 //Import Routes
 const auctionRoutes = require("./Application/Auction Listing/routes/auctions.js");
 const bidRouts = require("./Application/Auction Listing/routes/bid.js");
 // const adminRouter = require("./Application/AdminPortal/routes/adminRoute.js");
->>>>>>> Stashed changes
 
 const app = express();
 
@@ -19,15 +15,10 @@ app.use(express.json());
 app.use(cors());
 connectDB();
 
-<<<<<<< Updated upstream
-//adminPortalMiddleware
- app.use("/admin", adminRouter);
-=======
 //Route Middleware
 // app.use("/admin", adminRouter);
 app.use("/auctions", auctionRoutes);
 app.use("/bids", bidRouts);
->>>>>>> Stashed changes
 
 //errorHandler
 app.use(errorHandler);

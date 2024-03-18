2352fea810fc032bc498f874d1f822113604cdc4
Movindu Imeth Liyanage <142730691+Movin21@users.noreply.github.com>
2024-03-19 01:26:27 +0530

    Home Page /Layouts / Configurations

--- Frontend/src/components/ui/Shared/NavBar.tsx
+++ Frontend/src/components/ui/Shared/NavBar.tsx
@@ -1,5 +1,225 @@
-const NavBar = () => {
-  return <div>NavBar</div>;
-};
+import { Link } from "react-router-dom"; // Import Link from React Router
+
+import Logo from "../../../assets/Logo/Logo.png";
+
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card";
+
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
+
+function NavBar() {
+  return (
+    <div>
+      {/* Upper Navbar */}
+      <div className="flex justify-between items-center px-4 py-2 border-b border-gray-200 bg-gray-100">
+        <div>
+          <Link to="/">
+            {" "}
+            {/* Use Link instead of anchor */}
+            <img
+              src={Logo}
+              alt="RelicRoom Logo"
+              className="max-w-[10rem] w-full"
+            />
+          </Link>
+        </div>
+        {/* Create account, Login and Profile Avatar */}
+        <div className="flex items-center space-x-4">
+          <Menubar>
+            <MenubarMenu>
+              <div className="font-akshar text-primary font-bold ">
+                <MenubarTrigger className="border-none text-lg cborder-none hover:none">
+                  <Link
+                    to="#"
+                    className="hover:underline transition-all duration-300 text-medium"
+                  >
+                    Create Account
+                  </Link>
+                </MenubarTrigger>
+              </div>
+              <MenubarContent>
+                <MenubarItem
+                  className={`font-akshar text-primary hover:text-secondary `}
+                >
+                  <Link to="#">Type 1</Link>
+                </MenubarItem>
+                <MenubarSeparator />
+                <MenubarItem
+                  className={`font-akshar text-primary hover:text-secondary `}
+                >
+                  <Link to="#">Type 2</Link>
+                </MenubarItem>
+                <MenubarSeparator />
+                <MenubarItem
+                  className={`font-akshar text-primary hover:text-secondary `}
+                >
+                  <Link to="#">Type 3</Link>
+                </MenubarItem>
+                <MenubarSeparator />
+                <MenubarItem
+                  className={`font-akshar text-primary hover:text-secondary `}
+                >
+                  <Link to="#">Type 4</Link>
+                </MenubarItem>
+              </MenubarContent>
+            </MenubarMenu>
+          </Menubar>
+          <div className="font-akshar text-primary text-medium">
+            <HoverCard>
+              <HoverCardTrigger>
+                <Link to="#" className="hover:underline text-medium">
+                  Login
+                </Link>
+              </HoverCardTrigger>
+              <HoverCardContent>Login to the Account</HoverCardContent>
+            </HoverCard>
+          </div>
+          <Avatar>
+            <Link to="#">
+              <AvatarImage src="https://github.com/shadcn.png" />
+            </Link>
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+        </div>
+      </div>
+      {/* Second Navbar  */}
+      <div className="flex justify-between items-center px-4 py-2">
+        {/* Navigation bar */}
+        <NavigationMenu className="border-none text-lg cborder-none">
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuLink
+                className={`font-akshar text-primary hover:text-secondary hover:bg-white ${navigationMenuTriggerStyle()}`}
+              >
+                <Link to="#">Auctions</Link>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuLink
+                className={`font-akshar text-primary hover:text-secondary hover:bg-white ${navigationMenuTriggerStyle()}`}
+              >
+                <Link to="#">Customer Care</Link>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+            <DropdownMenu>
+              <DropdownMenuTrigger
+                className={`font-akshar text-primary hover:text-secondary hover:bg-white `}
+              >
+                <Link
+                  className={`font-akshar text-primary hover:text-secondary hover:bg-white ${navigationMenuTriggerStyle()}`}
+                  to=""
+                >
+                  Services
+                  <span>
+                    <div
+                      className={`ml-2 transition-all  group-hover:rotate-180`}
+                    >
+                      <svg
+                        width="15"
+                        height="15"
+                        viewBox="0 0 15 15"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M4.18179 6.18181C4.35753 6.00608 4.64245 6.00608 4.81819 6.18181L7.49999 8.86362L10.1818 6.18181C10.3575 6.00608 10.6424 6.00608 10.8182 6.18181C10.9939 6.35755 10.9939 6.64247 10.8182 6.81821L7.81819 9.81821C7.73379 9.9026 7.61934 9.95001 7.49999 9.95001C7.38064 9.95001 7.26618 9.9026 7.18179 9.81821L4.18179 6.81821C4.00605 6.64247 4.00605 6.35755 4.18179 6.18181Z"
+                          fill="currentColor"
+                          fill-rule="evenodd"
+                          clip-rule="evenodd"
+                        ></path>
+                      </svg>
+                    </div>
+                  </span>
+                </Link>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent
+                className={`font-akshar text-primary hover:text-secondary `}
+              >
+                <DropdownMenuSeparator />
+                <DropdownMenuItem
+                  className={`font-akshar text-primary hover:text-secondary `}
+                >
+                  Service 1
+                </DropdownMenuItem>
+                <DropdownMenuItem
+                  className={`font-akshar text-primary hover:text-secondary `}
+                >
+                  Service 2
+                </DropdownMenuItem>
+                <DropdownMenuItem
+                  className={`font-akshar text-primary hover:text-secondary `}
+                >
+                  Service 3
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+            <NavigationMenuItem>
+              <NavigationMenuLink
+                className={`font-akshar text-primary hover:text-secondary hover:bg-white ${navigationMenuTriggerStyle()}`}
+              >
+                <Link to="#">About Us</Link>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+        {/* Search Bar */}
+        <div className="relative flex items-center">
+          <input
+            type="text"
+            placeholder="Search here.."
+            className="flex-1 w-50 sm:w-60 transition-all duration-300 border border-gray-300 px-4 py-1 focus:outline-none focus:border-1 focus:border-primary hover:w-48 sm:hover:w-80"
+          />
+          <div className="absolute inset-y-0 right-0 flex items-center pr-3 text-gray-500 group-hover:text-primary">
+            <Link to="">
+              <svg
+                width="15"
+                height="15"
+                viewBox="0 0 15 15"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M10 6.5C10 8.433 8.433 10 6.5 10C4.567 10 3 8.433 3 6.5C3 4.567 4.567 3 6.5 3C8.433 3 10 4.567 10 6.5ZM9.30884 10.0159C8.53901 10.6318 7.56251 11 6.5 11C4.01472 11 2 8.98528 2 6.5C2 4.01472 4.01472 2 6.5 2C8.98528 2 11 4.01472 11 6.5C11 7.56251 10.6318 8.53901 10.0159 9.30884L12.8536 12.1464C13.0488 12.3417 13.0488 12.6583 12.8536 12.8536C12.6583 13.0488 12.3417 13.0488 12.1464 12.8536L9.30884 10.0159Z"
+                  fill="currentColor"
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default NavBar;
---
+++ Frontend/src/pages/Home/carousel.tsx
@@ -0,0 +1,131 @@
+import { useState, useEffect } from "react";
+import image1 from "../../assets/carousel/1.png";
+import image2 from "../../assets/carousel/2.png";
+import image3 from "../../assets/carousel/3.png";
+import image4 from "../../assets/carousel/4.jpeg";
+import image5 from "../../assets/carousel/5.jpeg";
+
+function Carousel() {
+  const slides = [
+    { id: 1, url: image1 },
+    { id: 2, url: image2 },
+    { id: 3, url: image3 },
+    { id: 4, url: image4 },
+    { id: 5, url: image5 },
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const prevSlide = () => {
+    const newIndex = (currentIndex - 1 + slides.length) % slides.length;
+    setCurrentIndex(newIndex);
+  };
+
+  const nextSlide = () => {
+    const newIndex = (currentIndex + 1) % slides.length;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToSlide = (slideIndex: number) => {
+    setCurrentIndex(slideIndex);
+  };
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      nextSlide();
+    }, 5000); // Switch slides every 5 seconds
+
+    return () => clearInterval(intervalId);
+  }, [currentIndex]);
+
+  return (
+    <div className="relative w-full">
+      {/* Slideshow */}
+      <div className="overflow-hidden h-[611px] relative">
+        {slides.map((slide, index) => (
+          <div
+            key={slide.id}
+            className="absolute inset-0 w-full h-full transition-opacity duration-500"
+            style={{
+              opacity: index === currentIndex ? 1 : 0,
+            }}
+          >
+            <img
+              src={slide.url}
+              alt={`Slide ${slide.id}`}
+              className="w-full h-full object-cover"
+            />
+          </div>
+        ))}
+      </div>
+      {/* Navigation buttons */}
+      <div className="absolute top-1/2 transform -translate-y-1/2 flex justify-between w-full">
+        <button
+          onClick={prevSlide}
+          className="text-white text-2xl rounded-full p-2 bg-black/20 cursor-pointer"
+        >
+          <svg
+            width="15"
+            height="15"
+            viewBox="0 0 15 15"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M8.84182 3.13514C9.04327 3.32401 9.05348 3.64042 8.86462 3.84188L5.43521 7.49991L8.86462 11.1579C9.05348 11.3594 9.04327 11.6758 8.84182 11.8647C8.64036 12.0535 8.32394 12.0433 8.13508 11.8419L4.38508 7.84188C4.20477 7.64955 4.20477 7.35027 4.38508 7.15794L8.13508 3.15794C8.32394 2.95648 8.64036 2.94628 8.84182 3.13514Z"
+              fill="currentColor"
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </button>
+        <button
+          onClick={nextSlide}
+          className="text-white text-2xl rounded-full p-2 bg-black/20 cursor-pointer"
+        >
+          <svg
+            width="15"
+            height="15"
+            viewBox="0 0 15 15"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M6.1584 3.13508C6.35985 2.94621 6.67627 2.95642 6.86514 3.15788L10.6151 7.15788C10.7954 7.3502 10.7954 7.64949 10.6151 7.84182L6.86514 11.8418C6.67627 12.0433 6.35985 12.0535 6.1584 11.8646C5.95694 11.6757 5.94673 11.3593 6.1356 11.1579L9.565 7.49985L6.1356 3.84182C5.94673 3.64036 5.95694 3.32394 6.1584 3.13508Z"
+              fill="currentColor"
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </button>
+      </div>
+      {/* Dots */}
+      <div className="absolute bottom-4 flex justify-center w-full ">
+        {slides.map((slide, slideIndex) => (
+          <div
+            key={slide.id}
+            onClick={() => goToSlide(slideIndex)}
+            className={`text-3xl cursor-pointer ${
+              currentIndex === slideIndex ? "text-footertxt" : "text-white"
+            }`}
+          >
+            <svg
+              width="15"
+              height="15"
+              viewBox="0 0 15 15"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M9.875 7.5C9.875 8.81168 8.81168 9.875 7.5 9.875C6.18832 9.875 5.125 8.81168 5.125 7.5C5.125 6.18832 6.18832 5.125 7.5 5.125C8.81168 5.125 9.875 6.18832 9.875 7.5Z"
+                fill="currentColor"
+              ></path>
+            </svg>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Carousel;

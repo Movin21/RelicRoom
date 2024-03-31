fd0af5a4d81e1430bc826982c1195802e2652894
wasathjayasundara7 <wasathjayasundara@gmail.com>
2024-03-31 12:23:39 +0530

    bidder

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -10,12 +10,18 @@ import AdminPortal from "./pages/AdminPortal/AdminPortal.tsx";
 import AdminPortalLogin from "./pages/AdminPortal/AdminPortalLogin.tsx";
 import AdminLoginLayout from "./pages/layouts/AdminLoginLayout.tsx";
 import AdminRegister from "./pages/AdminPortal/AdminRegister.tsx";
+import BidderLogin from "./pages/Bidder/BidderLogin.tsx";
+import BidderSignup from "./pages/Bidder/BidderSignup.tsx";
 const firebaseConfig = {};
 
 firebase.initializeApp(firebaseConfig);
 
 const router = createBrowserRouter([
-  { element: <RootLayout />, children: [{ path: "/", element: <Home /> }] },
+  { element: <RootLayout />, children: [{ path: "/", element: <Home /> },
+    {path: "/bidderLogin", element: <BidderLogin />},
+    {path: "bidderSignup", element: <BidderSignup/>}
+   ],
+  },
   {
     element: <AdminLayout />,
     children: [

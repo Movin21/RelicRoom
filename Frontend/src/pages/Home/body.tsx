ec8e95cd8af25f0340d03d79d8a1360a2ea89663
Yasas Lakmina <138884690+YasasLakmina@users.noreply.github.com>
2024-04-16 11:34:07 +0530

    home page design 3

--- Frontend/src/pages/Home/body.tsx
+++ Frontend/src/pages/Home/body.tsx
@@ -3,10 +3,12 @@ import React from "react";
 import Section3 from "./Section3";
 import Section2 from "./Section2";
 import Section1 from "./Section1";
+import Section4 from "./Section4";
 
 function Body() {
   return (
     <div>
+      <Section4 />
       <Section1 />
       <Section2 />
       <Section3 />
